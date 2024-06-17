60f022dec28cd9baf0b4e701e6553ec5e067abb5
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-06-17 18:37:43 +0530

    nothing

--- src/modules/contactus/index.js
+++ src/modules/contactus/index.js
@@ -39,29 +39,9 @@ export default function Contactus() {
               <div className="contact-grid2-content">
                 <h2 data-aos="fade-up" data-aos-duration="1000">Contact Us!</h2>
                 <form onSubmit={handleSubmit}>
-                  <input
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    placeholder="Enter your Name"
-                    required
-                  />
-                  <input
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    placeholder="Enter your Email"
-                    required
-                  />
-                  <textarea
-                    name="message"
-                    value={formData.message}
-                    onChange={handleChange}
-                    placeholder="Enter your message"
-                    required
-                  ></textarea>
+                  <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Enter your Name" required />
+                  <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Enter your Email" required />
+                  <textarea name="message" value={formData.message} onChange={handleChange} placeholder="Enter your message" required></textarea>
                   <div className="contact-grid2-button">
                     <button type="submit">Submit</button>
                   </div>
